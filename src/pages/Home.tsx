b847adee32226b4b7970ad46926ad660d7901ebf
isaacvianna <isaacsvianna@gmail.com>
2021-07-17 22:28:33 -0300

    criando context de auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
   return (
     <div id='pageAuth'>
       <aside>
@@ -18,7 +31,7 @@ export function Home() {
       <main>
         <div className='mainContent'>
           <img src={logoImg} alt='letmeask logo' />
-          <button className='createRoom'>
+          <button onClick={handleCreateRoom} className='createRoom'>
             <img src={googleIconImg} alt='google logo' />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+  const { user } = useAuth();
+
   return (
     <div id='pageAuth'>
       <aside>
@@ -24,7 +31,7 @@ export function NewRoom() {
           </form>
 
           <p>
-            Quer entrar em uma sala existente? <a href='#'>clique aqui</a>
+            Quer entrar em uma sala existente? <Link to='/'>clique aqui</Link>
           </p>
         </div>
       </main>
