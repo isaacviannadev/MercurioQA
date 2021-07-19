cd91dd7993f8a14f1ab6d27ecb37ebb71b702e37
isaacvianna <isaacsvianna@gmail.com>
2021-07-19 12:26:55 -0300

    novo projeto

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
